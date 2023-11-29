1f9afeef7abdbaee8ef25067d802369eff9a0bc7
Zenkie Bear <zq@zenkie.cn>
2023-11-29 14:59:24 +0800

    feat: finished part of GIN sections
    ASCII JSON, HTML Rendering, HTTP2 Server Push

---
+++ 11.gin/assets/app.js
@@ -0,0 +1,1 @@
+console.log('Salut, comment ça va?');
---
+++ 11.gin/example.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	setupPing(router)
+	setupAsciiJSON(router)
+	renderHTML(router)
+	serverPush(router)
+
+	// router.Run() // listen and serve on 0.0.0.0:8080
+	router.RunTLS("localhost:8080", "ssl.pem", "ssl.key")
+}
+
+func setupPing(router *gin.Engine) {
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+}
+
+func setupAsciiJSON(router *gin.Engine) {
+	router.GET("/someJSON", func(c *gin.Context) {
+		data := map[string]interface{}{
+			"lang": "GO 语言",
+			"tag":  "<br>",
+		}
+
+		// Output {"lang":"GO \u8bed\u8a00","tag":"\u003cbr\u003e"}
+		c.AsciiJSON(http.StatusOK, data)
+	})
+}
+
+func renderHTML(router *gin.Engine) {
+	// router.LoadHTMLFiles("templates/me.html")
+	router.LoadHTMLGlob("templates/**/*")
+	router.GET("/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "me.html", gin.H{
+			"title": "Bonjour! Je suis Zenkie Bear.",
+		})
+	})
+	router.GET("/posts/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "posts/index.html", gin.H{
+			"title": "Posts",
+		})
+	})
+	router.GET("/users/index", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "users/index.html", gin.H{
+			"title": "Users",
+		})
+	})
+}
+
+var html = template.Must(template.New("https").Parse(`
+<html>
+<head>
+  <title>Https Test</title>
+	<script src="/assets/app.js"></script>
+</head>
+<body>
+  <h1 style="color: red;">Bienvenue, Ginner!</h1>
+</html>
+`))
+
+func serverPush(router *gin.Engine) {
+	router.Static("/assets", "./assets")
+	router.SetHTMLTemplate(html)
+
+	router.GET("/", func(ctx *gin.Context) {
+		if pusher := ctx.Writer.Pusher(); pusher != nil {
+			if err := pusher.Push("/assets/app.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		ctx.HTML(http.StatusOK, "https", gin.H{
+			"status": "success",
+		})
+	})
+}
