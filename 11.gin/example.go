4fccd569f20895fae40eaafff6a91864f19cbedf
Zenkie Bear <zq@zenkie.cn>
2023-11-30 12:34:29 +0800

    feat: added test for main.go

--- 11.gin/example.go
+++ 11.gin/example.go
@@ -2,7 +2,8 @@ package main
 
 import "zenkie.cn/learn-gin/router"
 
-func main() {
+// todo rename it with "main"
+func start() {
 	r := router.Router()
 	// router.Run() // listen and serve on 0.0.0.0:8080
 	r.Run("localhost:8080")
